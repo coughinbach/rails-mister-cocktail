8fe1586f993270a82a93e9cdcfda0a16859cd328
Lola Barbier <lolabarbier@hotmail.com>
2017-11-24 21:47:46 +0100

    fixed footer scroll js <3

--- app/javascript/components/footer.js
+++ app/javascript/components/footer.js
@@ -1,7 +1,7 @@
 function initUpdateFooterOnScroll() {
   const footer = document.querySelector('.footer');
   window.addEventListener('scroll', () => {
-    if (window.scrollY <= window.innerHeight) {
+    if (window.scrollY <= 100) {
       footer.classList.add('footer-hide');
     } else {
       footer.classList.remove('footer-hide');
