62400ab48bb53e5f801d4692c8b3774530a886af
Lola Barbier <lolabarbier@hotmail.com>
2017-11-24 16:11:28 +0100

    nice random pictures for each cocktail, on card and in show view

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,9 +22,14 @@ ingredients["drinks"].each do |ingredient|
 end
 p "done"
 
-p "Creating cocktail names..."
-21.times do
+photos = ["http://res.cloudinary.com/dxlfgmaxy/image/upload/v1511535854/cocktail1_lbdqll.png", "http://res.cloudinary.com/dxlfgmaxy/image/upload/v1511535853/cocktail2_rxl1k3.png", "http://res.cloudinary.com/dxlfgmaxy/image/upload/v1511535852/cocktail3_xbehbu.png", "http://res.cloudinary.com/dxlfgmaxy/image/upload/v1511535852/cocktail4_c3ygiu.png", "http://res.cloudinary.com/dxlfgmaxy/image/upload/v1511535853/cocktail5_wozacf.png", "http://res.cloudinary.com/dxlfgmaxy/image/upload/v1511535852/cocktail6_smlb32.png", "http://res.cloudinary.com/dxlfgmaxy/image/upload/v1511535853/cocktail7_sb9xpi.png", "http://res.cloudinary.com/dxlfgmaxy/image/upload/v1511535853/cocktail8_wrnh59.png"]
+
+p "Creating cocktails"
+15.times do
   cocktail = Cocktail.new(name: Faker::Hipster.words(2, false, true).join(" ").capitalize)
+  cocktail.remote_photo_url = photos.sample
   cocktail.save
 end
 p "done-zo"
+
+
